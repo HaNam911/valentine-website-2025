93d0c5d928ee7a76c4ac11159e2c3de398c0e496
HaNam911 <101507669+HaNam911@users.noreply.github.com>
2025-02-09 21:19:54 +0100

    Update config.js

--- config.js
+++ config.js
@@ -27,31 +27,18 @@ const CONFIG = {
             noBtn: "Nee",                                               // Text for "No" button
             secretAnswer: "Nghỉ học để rảnh đi:)))"           // Secret hover message
         },
-        second: {
-            text: "How much do you love me?",                          // For the love meter
-            startText: "This much!",                                   // Text before the percentage
-            nextBtn: "Next ❤️"                                         // Text for the next button
-        },
-        third: {
+        Second: {
             text: "Will you be my Valentine on February 14th, 2025? 🌹", // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
     },
 
-    // Love meter messages
-    // They show up depending on how far they slide the meter
-    loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
-    },
-
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
-        emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
+        title: "Yay! Tun lucky qúa tr 🎉💝💖💝💓",
+        message: "See you soon!!!!",
+        emojis: "🧸🐻‍❄️🤗💝🐱❤️🐶"  // These will bounce around
     },
 
     // Color scheme for the website
